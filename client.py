c6352df473cf60552401310d1ad2a49d6f890127
zhou han <han.zhou@gu3.co.jp>
2014-02-13 18:13:16 +0900

    finish send message to group function

--- client.py
+++ client.py
@@ -4,17 +4,29 @@
 import urllib2, urllib
 import json
 import sys
-
+import simplejson
 
 class Client(object):
 
     SETTINGS_FILE = 'local.settings'
 
     def __init__(self):
-        pass
+        try:
+            settings_file_obj = open(self.SETTINGS_FILE)
+            data = json.load(settings_file_obj)
+        except Exception, e:
+            print "Read settings file error.", e
+            sys.exit(1)
 
-    def _init_settings(self):
-        pass
+        self.client_id = data['client_id']
+        self.request_url = data['redirect_url']
+        self.msg_format = data['msg_format']
+        self.target_group_list = data['target_group_list']
+        self.topic_list = data['topic_list']
+        self.access_token = data['access_token']
+
+    def __str__(self):
+        return u"{}-{}-{}-{}-{}".format(self.client_id, self.request_url, self.target_group_list, self.topic_list, self.access_token)
 
     @property
     def auth_url(self):
@@ -24,13 +36,41 @@ class Client(object):
     def profile_url(self):
         return "https://www.yammer.com/api/v1/users/current.json"
 
-    def get_group_id_list(self):
-        pass
+    @property
+    def post_message_url(self):
+        return "https://www.yammer.com/api/v1/messages.json"
 
-    def send_message_to_group(self):
-        pass
+    def get_group_list(self):
+        response = self._do_request(self.profile_url)
+        json_data = json.loads(response.read())
+        group_list = []
+        for item in json_data['web_preferences']['home_tabs']:
+            if item['type'] == 'group':
+                temp_dict = {}
+                temp_dict['group_id'] = item['group_id']
+                temp_dict['name'] = item['name']
+                group_list.append(temp_dict)
+        return group_list
+
+    def print_group_list(self):
+        group_list = self.get_group_list()
+        for group in group_list:
+            print group['group_id'],
+            print group['name']
+
+    def send_message_to_group(self, msg_part_1, msg_part_2):
+        msg = self.msg_format % (msg_part_1, msg_part_2)
+        for item in self.target_group_list:
+            for index, topic in enumerate(self.topic_list, start=1):
+                group_id = item["group_id"]
+                post_data = {}
+                post_data['body'] = msg.encode('utf-8')
+                post_data['group_id'] = group_id
+                post_data['topic{}'.format(index)] = topic['topic']
+
+                self._do_request(self.post_message_url, post_data)
         
-    def do_request(self, url, data=None):
+    def _do_request(self, url, data=None):
 
         request = urllib2.Request(url)
         request.add_header("Authorization", "Bearer %s" % self.access_token)
@@ -53,4 +93,10 @@ class Client(object):
         return response
             
 if __name__ == '__main__':
-    pass
+    # get args from command line
+    if len(sys.argv) != 3:
+        print "Argument number error, need two arguments."
+        sys.exit(1)
+
+    client = Client()
+    client.send_message_to_group(sys.argv[1], sys.argv[2])
